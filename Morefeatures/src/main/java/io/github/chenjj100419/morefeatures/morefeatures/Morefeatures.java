734a561feab60cd58f05807afdc9b67a66dde8b4
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2020-08-20 20:38:13 +0800

    update

---
+++ Morefeatures/src/main/java/io/github/chenjj100419/morefeatures/morefeatures/Morefeatures.java
@@ -0,0 +1,17 @@
+package io.github.chenjj100419.morefeatures.morefeatures;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Morefeatures extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
